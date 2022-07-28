47d800a7853bb0affb22b365295c6d95ec467db0
Coreforge <BC549b@gmail.com>
2022-07-28 23:29:01 +0200

    removed 0 from log levels to avoid interference with c strings

--- logger/logger.h
+++ logger/logger.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#define LOG_LEVEL_DEBUG 0
-#define LOG_LEVEL_INFO 1
-#define LOG_LEVEL_WARNING 2
-#define LOG_LEVEL_ERROR 3
-#define LOG_LEVEL_CRITICAL 4
+// starting with 0 could interfere with terminating null characters
+#define LOG_LEVEL_DEBUG 1
+#define LOG_LEVEL_INFO 2
+#define LOG_LEVEL_WARNING 3
+#define LOG_LEVEL_ERROR 4
+#define LOG_LEVEL_CRITICAL 5
 
 class Logger{
 public:
