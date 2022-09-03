f4477cd0aadd509523bfcce61c10c03f16501d5b
Coreforge <BC549b@gmail.com>
2022-09-03 03:03:47 +0200

    extended item class to make it useful

--- Item.cpp
+++ Item.cpp
@@ -5,12 +5,15 @@
 
 #include "Errors.h"
 
-#define DUMP_DATA_TABLE true
-#define DUMP_CONTENT_TABLE true
-#define DUMP_STRING_TABLE true
+#define DUMP_DATA_TABLE false
+#define DUMP_CONTENT_TABLE false
+#define DUMP_STRING_TABLE false
 
-Item::Item(uint8_t* data, uint32_t length, Logger* logger){
+Item::Item(uint8_t* data, uint32_t length, Logger* logger, std::string name, std::string path){
 
+	// copy the strings. Ideally, we don't want to depend on any data managed by another object
+	this->name = std::string(name);
+	this->path = std::string(path);
 	this->logger = logger;
 	// create a copy of the file. By doing this I don't need to keep track of which extracted buffer is still needed and which one can be freed
 	// and it should only momentarily increase memory usage, unless an item gets opened multiple times without freeing it again first
@@ -57,4 +60,11 @@ Item::Item(uint8_t* data, uint32_t length, Logger* logger){
 			logger->log(LOG_LEVEL_DEBUG,"%s\n",stringTable.invalidStrings[i].string.c_str());
 		}
 	}
+}
+
+void* Item::getDataBlock(DataTableEntry* entry){
+	// get the full offset in the file. This depends on the region the data block is in
+	uint32_t full_offset = entry->region == 1 ? header.dataOffset + entry->offset : header.data2Offset + entry->offset;
+
+	return data + full_offset;
 }
--- Item.h
+++ Item.h
@@ -6,6 +6,8 @@
 #include "StringTable.h"
 #include "logger/logger.h"
 
+#include <string>
+
 class Item{
 
 public:
@@ -15,8 +17,14 @@ public:
 	StringTable stringTable;
 	Logger* logger;
 
+	std::string name;
+	std::string path;
+
 	// the data will be copied into a new buffer,
-	Item(uint8_t* data, uint32_t length, Logger* logger);
+	Item(uint8_t* data, uint32_t length, Logger* logger, std::string name, std::string path);
+
+	// returns a pointer to the start of this blocks data. The data does not get copied again, so this pointer is only valid as long as this item exists
+	void* getDataBlock(DataTableEntry* entry);
 
 
 private:
